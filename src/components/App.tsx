c2f3463a6906395cecbca9fce6deb62f16bb5ffc
ClaytonDewey <clay@dryadmedia.com>
2023-07-22 12:27:23 -0500

    Move App to components directory

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import GlobalStyle from '../theme/global';
+
+function App() {
+  return (
+    <div className='App'>
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
