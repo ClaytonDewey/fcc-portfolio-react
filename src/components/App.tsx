a74652aa4e28d03f2849459f019aeb7cd83403c4
ClaytonDewey <clay@dryadmedia.com>
2023-07-22 12:54:26 -0500

    Import and display Intro & Services components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import Header from './Header';
 import GlobalStyle from '../theme/global';
+import Intro from './Intro';
+import Services from './Services';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Header />
+      <Intro />
+      <Services />
     </div>
   );
 }
